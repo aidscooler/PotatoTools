1fd7e6e7ee3e3cd0922b8de1dac90b0fa384e8b5
BlingYang <20971840@qq.com>
2024-11-10 23:42:47 +0800

    增加json和yaml互转工具

---
+++ .vitepress/components/tools/devTools/JSON-YamlConverter.vue
@@ -0,0 +1,143 @@
+<template>
+    <el-card class="converter-card">
+      <template #header>
+        <div class="card-header">
+          <span>JSON与YAML相互转换工具</span>
+        </div>
+      </template>
+      <el-tabs v-model="activeTab">
+        <el-tab-pane label="JSON to YAML" name="json-to-yaml">
+          <div class="converter-container">
+            <el-input
+              type="textarea"
+              v-model="jsonInput"
+              placeholder="Enter JSON here"
+              class="input-area"
+              :rows="20"
+            ></el-input>
+            <div class="output-area" v-html="yamlOutputHtml">           
+            </div>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="YAML to JSON" name="yaml-to-json">
+          <div class="converter-container">
+            <el-input
+              type="textarea"
+              v-model="yamlInput"
+              placeholder="Enter YAML here"
+              class="input-area"
+              :rows="20"
+            ></el-input>
+            <div class="output-area" v-html="jsonOutputHtml"></div>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+</template>
+  
+<script setup lang="ts">
+  import JSON5 from 'json5';
+  import hljs from 'highlight.js/lib/core';
+  import jsonHljs from 'highlight.js/lib/languages/json';
+  import yamlHljs from 'highlight.js/lib/languages/yaml';
+  import 'highlight.js/styles/atom-one-light.css'; /* 你可以选择其他样式，如 'github.css' */
+  import { parse as parseYaml, stringify as stringifyYaml } from 'yaml';
+  
+  const activeTab = ref('json-to-yaml');
+  const jsonInput = ref('');
+  const yamlInput = ref('');
+  const yamlOutputHtml = ref('');
+  const jsonOutputHtml = ref('');
+
+  hljs.registerLanguage('json', jsonHljs);
+  hljs.registerLanguage('yaml', yamlHljs);
+  
+  const yamlOutput = computed(() => {
+    const jsonstring = jsonInput.value.trim();
+    if (!jsonstring) {
+      return '';
+    } else {
+      try {
+        const obj = JSON5.parse(jsonInput.value);
+        return stringifyYaml(obj);
+      } catch (e) {
+        return 'Error: ' + e.message;
+      }
+    }
+  });
+  
+  const jsonOutput = computed(() => {
+    const yamlstring = yamlInput.value.trim();
+    if (!yamlstring) {
+      return '';
+    } else {
+      try {
+        const obj = parseYaml(yamlInput.value, { merge: true });
+        return JSON.stringify(obj, null, 3);
+      } catch (e) {
+        return 'Error: ' + e.message;
+      }
+    }
+  });
+  
+  watch(yamlOutput, (newValue) => {
+    yamlOutputHtml.value = hljs.highlight(newValue, { language: 'yaml' }).value;
+  },{ immediate: true });  
+
+  watch(jsonOutput, (newValue) => {
+    jsonOutputHtml.value = hljs.highlight(newValue, { language: 'json' }).value;
+  },{ immediate: true });    
+  </script>
+  
+<style scoped>
+  .converter-card {
+    max-width: 1000px;
+    margin: 0px auto;
+    padding: 20px;
+  }
+  .converter-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+  .input-area,
+  .output-area {
+    width: 48%;
+    margin: 10px 0;
+    font-family: monospace;
+    font-size: 14px;
+    line-height: 1.5;
+  }
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .output-area {
+    white-space: pre-wrap;
+    text-align: left;
+    border: 1px solid #ccc;
+    padding: 10px;
+    overflow: auto;
+    background-color: #f5f5f5;
+    height: calc(20 * 1.5em + 10px); /* 根据行高和行数计算高度 */ 
+  }
+  
+  /* 自定义滚动条样式 */
+  .output-area::-webkit-scrollbar {
+    width: 8px;
+  }
+  .output-area::-webkit-scrollbar-thumb {
+    background-color: #999;
+    border-radius: 4px;
+  }
+  .output-area::-webkit-scrollbar-track {
+    background-color: #f1f1f1;
+    border-radius: 4px;
+  }
+  .output-area::-webkit-scrollbar-thumb:hover {
+    background-color: #666;
+  }
+  </style>
+  
